0732d6b718c45aa782642273b2f6ff2de2fb9ac7
David Stavis <davidstavis@gmail.com>
2022-05-29 18:04:13 -0600

    Mark cells complete and persist across sessions

--- README.md
+++ README.md
@@ -13,3 +13,18 @@ Features:
 - the user can write text on a bingo box that is separate from the fact in that box. this text field can be used for, at the very least, writing the name of the person they think the fact corresponds to
 
 - the background is the canva art designed by Emily
+
+// Goal: Display some user input (i.e. user's name shows up over the card)
+// Goal: let a user mark a cell complete/incomplete and display it differently accordingly
+// Goal: some CYOA, i.e. localStorage
+// login feature would count as CYOA
+
+// 1. A unique user name owns a unique card object
+// 2. the user name submission should enforce that it is a first name and a last name, and that it is capitalized properly
+// 3. the form should inform the user of what format it expects them to enter their name in
+// 4. a card object knows which data it has and what order that data is in (it should be a random order unique to each card, but it is not necessary to enforce uniqueness)
+// 5. eventually a card object will know which cells have been "solved"
+// 6. eventually the app will notice if the card has boxes solved that happen to form a bingo victory condition (i.e. 4 diagonally, 4 across, 4 in a column)
+// 7. a card object knows which username it belongs to
+// 8. when a user submits a name that we haven't seen before, we generate a fresh card just for them. we make sure that card knows it belongs to them so that the next time they provide the same name they can get the same card
+// 9. when provided the user's name, the application retrieves the card that was assigned to that name, and it's the same card (with the same contents in the same order) each time
--- main.js
+++ main.js
@@ -1,16 +1,20 @@
+// ************ SETTINGS ************
+
 const numberOfRows = 4;
 var currentCard = undefined;
 
-// QuerySelectors and Event Listeners
+// ************ end SETTINGS ************
+
+// ************ QuerySelectors and Event Listeners ************
 
 var usernameSubmitButton = document.querySelector('#user-ID button')
 var usernameTextInput = document.querySelector('input#username')
 
-usernameSubmitButton.addEventListener('click', usernameClickHandler)
+// usernameSubmitButton.addEventListener('click', usernameClickHandler)
 
 function usernameClickHandler(eventObject){
   createNewUser(usernameTextInput.value)
-  // TODO: erase value
+  // TODO: erase form input value
   // TODO: update message to indicate that we now have a user
 }
 
@@ -18,6 +22,9 @@ function usernameClickHandler(eventObject){
 // Restore ID in localStorage using ID username?
 // Let the user switch which card from storage by inputting a username that's already stored?
 
+// ************ end QuerySelectors and Event Listeners ************
+
+// ************ VIEW ****************
 
 function displayBoxes(numberOfRows){
   var tableBody = document.querySelector("#box-holder tbody")
@@ -36,17 +43,44 @@ function displayBoxes(numberOfRows){
   }
 }
 
+function placeEventListeners(){
+  var cells = document.querySelectorAll("td.bingo-box")
+  for(var i = 0;i<cells.length;i++){
+    cells[i].addEventListener("click", markBoxComplete)
+  }
+
+}
+
+function markBoxComplete(eventObject){
+  var cellIndex = eventObject.currentTarget.children[0].children[0].id
+  currentCard.cells[cellIndex].complete = true;
+  updateLocalStorageUser()
+  updateBoxContents(currentCard)
+}
+
+function updateLocalStorageUser(){
+  localStorage.setItem(currentCard.username, JSON.stringify(currentCard))
+}
+
 function updateBoxContents(card){
   var cells = document.querySelectorAll(".bingo-box span")
 
   for(var i = 0; i < cells.length; i++){
     cells[i].innerText = card.cells[i].content
+    cells[i].id = card.cells[i].index
+    if(card.cells[i].complete){
+      cells[i].parentElement.parentElement.classList.add("complete")
+    }
   }
 }
+// **************** end VIEW ****************
+
+// **************** USER MANAGER ****************
 
 function createNewUser(username){
   var newCard = new Card(username, numberOfRows)
   localStorage.setItem(username, JSON.stringify(newCard))
+  updateLatestUser(username)
   currentCard = newCard;
   updateBoxContents(currentCard)
 }
@@ -55,26 +89,31 @@ function loadUser(username){
   var userCard = JSON.parse(localStorage[username])
   currentCard = userCard
   updateBoxContents(currentCard)
+  updateLatestUser(username)
 }
 
+function updateLatestUser(username){
+  localStorage.setItem('latestUser', username)
+}
+
+function loadLatestUser(){
+  var username = localStorage.getItem('latestUser')
+  if(username){
+    var userCard = JSON.parse(localStorage.getItem(username))
+    currentCard = userCard
+    updateBoxContents(currentCard)
+  }
+}
+
+// **************** end USER MANAGER ****************
+
+// **************** APPLICATION MANAGER ****************
 function start(){
   displayBoxes(numberOfRows)
+  loadLatestUser()
+  placeEventListeners()
 }
 
 start()
 
-
-// Goal: Display some user input (i.e. user's name shows up over the card)
-// Goal: let a user mark a cell complete/incomplete and display it differently accordingly
-// Goal: some CYOA, i.e. localStorage
-// login feature would count as CYOA
-
-// 1. A unique user name owns a unique card object
-// 2. the user name submission should enforce that it is a first name and a last name, and that it is capitalized properly
-// 3. the form should inform the user of what format it expects them to enter their name in
-// 4. a card object knows which data it has and what order that data is in (it should be a random order unique to each card, but it is not necessary to enforce uniqueness)
-// 5. eventually a card object will know which cells have been "solved"
-// 6. eventually the app will notice if the card has boxes solved that happen to form a bingo victory condition (i.e. 4 diagonally, 4 across, 4 in a column)
-// 7. a card object knows which username it belongs to
-// 8. when a user submits a name that we haven't seen before, we generate a fresh card just for them. we make sure that card knows it belongs to them so that the next time they provide the same name they can get the same card
-// 9. when provided the user's name, the application retrieves the card that was assigned to that name, and it's the same card (with the same contents in the same order) each time
+// **************** end APPLICATION MANAGER ****************
